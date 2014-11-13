081923c6febae91397590af290732a6248916d35
mbartlet <Mary.Bartlett@nokia.com>
2014-11-13 16:26:22 +0000

    26: adding extra checks to test

--- src/test/formatCurrency-test.js
+++ src/test/formatCurrency-test.js
@@ -24,17 +24,30 @@ describe('formatCurrency', function () {
 
     it('should throw an error when no value is given to currency', function () {
         expect(function () {formatCurrency();}).toThrow();
+        expect(localiserScope._i18n.toCurrency).not.toHaveBeenCalled();
     });
 
     it('should throw an error when given a string that is not a number', function () {
         expect(function () {formatCurrency("abc");}).toThrow();
+        expect(localiserScope._i18n.toCurrency).not.toHaveBeenCalled();
     });
 
-    it('should return false when territory is "default"', function () {
+    it('should return false and not call i18n.toCurrency when territory is "default"', function () {
         localiserScope._territory = 'default';
 
         expect(formatCurrency.call(localiserScope, '10')).toBe(false);
+        expect(localiserScope._i18n.toCurrency).not.toHaveBeenCalled();
+
     });
+
+    it('should call i18n.toCurrency when territory is not "default"', function () {
+        localiserScope._territory = 'gb';
+        formatCurrency.call(localiserScope, 10);
+
+        expect(localiserScope._i18n.toCurrency).toHaveBeenCalledWith
+        (10, {format: localiserScope._i18n.currencyFormat, unit: localiserScope._i18n.currencySymbol, 
+            precision: localiserScope._i18n.currencyPrecision, separator: localiserScope._i18n.currencySeparator, delimiter: localiserScope._i18n.currencyDelimiter});
+    });    
 
     it('should call i18n.toCurrency when given a number', function () {
         formatCurrency.call(localiserScope, 10);
--- src/test/formatNumber-test.js
+++ src/test/formatNumber-test.js
@@ -21,10 +21,13 @@ describe('formatNumber', function () {
 
     it('should throw an error when no value is given to number', function () {
         expect(function () {formatNumber();}).toThrow();
+        expect(localiserScope._i18n.toNumber).not.toHaveBeenCalled();
+
     });
 
     it('should throw an error when given a string that is not a number', function () {
         expect(function () {formatNumber("abc");}).toThrow();
+        expect(localiserScope._i18n.toNumber).not.toHaveBeenCalled();
     });
 
     it('should call i18n.toNumber when given a number', function () {
