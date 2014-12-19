1624c9a4cae133cf2bb406d7a043b9dcda351ab1
mbartlet <Mary.Bartlett@nokia.com>
2014-12-19 15:44:52 +0000

    adding gh-pages as part of release job

--- grunt-tasks/release.js
+++ grunt-tasks/release.js
@@ -9,5 +9,6 @@ module.exports = function (grunt) {
         // increment the version number of the package
         grunt.task.run('bump');
         grunt.task.run('jsdoc');
+        grunt.task.run('gh-pages');
     });
 };
